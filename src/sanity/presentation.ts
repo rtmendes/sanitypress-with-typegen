bf543b0d5c70abad6563ff38d5eb1dfb7021f50a
Mitchell Christ <mitchell@human.marketing>
2025-09-15 16:44:37 -0700

    add presentation document resolvers

--- src/sanity/presentation.ts
+++ src/sanity/presentation.ts
@@ -1,9 +1,47 @@
-import { presentationTool } from 'sanity/presentation'
+import { presentationTool, defineLocations } from 'sanity/presentation'
+import { groq } from 'next-sanity'
+import { BLOG_DIR } from '@/lib/env'
 
 export default presentationTool({
 	previewUrl: {
 		previewMode: {
 			enable: '/api/draft-mode/enable',
+		},
+	},
+	resolve: {
+		locations: {
+			page: defineLocations({
+				select: {
+					title: 'title',
+					slug: 'metadata.slug.current',
+				},
+				resolve: (doc) => ({
+					locations: [
+						{
+							title: doc?.title,
+							href: doc?.slug
+								? doc.slug === 'index'
+									? '/'
+									: `/${doc.slug}`
+								: '/',
+						},
+					],
+				}),
+			}),
+			'blog.post': defineLocations({
+				select: {
+					title: 'metadata.title',
+					slug: 'metadata.slug.current',
+				},
+				resolve: (doc) => ({
+					locations: [
+						{
+							title: doc?.title,
+							href: doc?.slug ? `/${BLOG_DIR}/${doc.slug}` : `/${BLOG_DIR}`,
+						},
+					],
+				}),
+			}),
 		},
 	},
 })
