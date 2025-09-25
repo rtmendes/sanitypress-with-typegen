820bdeb5b58d06303094ad6115aff4ddd8f729ed
Mitchell Christ <mitchell@human.marketing>
2025-09-24 18:31:45 -0700

    fix mobile menu gap issue

--- src/ui/header/index.tsx
+++ src/ui/header/index.tsx
@@ -12,7 +12,7 @@ export default async function () {
 
 	return (
 		<Wrapper className="bg-background/50 sticky top-0 z-10 backdrop-blur">
-			<div className={cn(css.root, 'section grid items-center gap-4 p-4')}>
+			<div className={cn(css.root, 'section grid items-center gap-x-4 p-4')}>
 				<Link href="/" className="font-bold [grid-area:logo]">
 					{site?.title}
 				</Link>
